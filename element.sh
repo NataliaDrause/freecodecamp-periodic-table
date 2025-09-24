b1729f3e3558dea8c459362373acb14deaea5e2d
Natalia Drause <nataliadrause@gmail.com>
2025-09-24 12:44:38 -0400

    feat: add query and message

--- element.sh
+++ element.sh
@@ -11,4 +11,22 @@ fi
 # Argument is provided
 ELEMENT=$1
 
-echo $1
+# Query the database for the element
+ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
+FROM elements e
+JOIN properties p ON e.atomic_number = p.atomic_number
+JOIN types t ON p.type_id = t.type_id
+WHERE e.symbol = '$ELEMENT' OR e.name = '$ELEMENT';")
+
+# Check if the query returned a result
+if [[ -z $ELEMENT_INFO ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+# Read the fields into variables
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT_INFO"
+
+# Output formatted message
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
